ea4d05e52ce50e74c05c194b82f7b344a5d68058
unknown <tsikatawill@gmail.com>
2021-12-18 01:16:32 +0000

    finished up

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const reouter = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3001;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
